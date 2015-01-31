9ab516572f752bd4a336b6d098896154d5276049
Cythilya Tang <cythilya@gmail.com>
2015-01-31 11:23:27 +0800

    wireframe

---
+++ Dotchi/Dotchi/Content/dotchi/wireframe/wireframe/more_files/axurerp_pagespecificscript.js
@@ -0,0 +1,29 @@
+﻿for(var i = 0; i < 77; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u68'] = 'center';gv_vAlignTable['u76'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u62'] = 'center';gv_vAlignTable['u53'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u60'] = 'center';gv_vAlignTable['u64'] = 'center';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u11'] = 'center';document.getElementById('u41_img').tabIndex = 0;
+
+u41.style.cursor = 'pointer';
+$axure.eventManager.click('u41', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('fb_user.html');
+
+}
+});
+gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u66'] = 'center';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u52'] = 'top';gv_vAlignTable['u43'] = 'top';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u50'] = 'top';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u54'] = 'top';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u56'] = 'center';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u42'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u72'] = 'center';gv_vAlignTable['u18'] = 'top';document.getElementById('u37_img').tabIndex = 0;
+
+u37.style.cursor = 'pointer';
+$axure.eventManager.click('u37', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('fb_user.html');
+
+}
+});
+gv_vAlignTable['u58'] = 'center';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u70'] = 'center';gv_vAlignTable['u73'] = 'top';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u4'] = 'top';
