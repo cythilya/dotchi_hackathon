fb77934eeed2df79619c464c86ff3b3317cead0c
Cythilya Tang <cythilya@gmail.com>
2015-01-31 10:16:50 +0800

    wireframe

---
+++ Dotchi/Dotchi/Content/dotchi/wireframe/html/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o)])]);}; 
+var b="rootNodes",c="pageName",d="Home",e="type",f="Wireframe",g="url",h="Home.html",i="children",j="Page 1",k="Page_1.html",l="Page 2",m="Page_2.html",n="Page 3",o="Page_3.html";
+return _creator();
+})();
