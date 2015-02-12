bdfae432eb8e37d3acd5321283aa2b62a069192c
Cythilya Tang <cythilya@gmail.com>
2015-02-12 22:36:02 +0800

    #4 create food area, add mvc files

---
+++ Dotchi/Dotchi/Areas/Food/Controllers/FoodController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dotchi.Areas.Food.Controllers
+{
+    public class FoodController : Controller
+    {
+        //
+        // GET: /Food/Food/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dotchi/Dotchi/Areas/Food/FoodAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Dotchi.Areas.Food
+{
+    public class FoodAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Food";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Food_default",
+                "Food/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
